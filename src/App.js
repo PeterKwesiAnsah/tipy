89257a61db7e16e22c6f3a86ec4b2cb79ca68019
Ansah1234 <ansahpeter123@yaoo.com>
2020-10-14 17:34:20 +0800

    page svg backgroud complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import Signin from './components/Signin'
+import {Route,Switch} from 'react-router-dom'
+import './App.scss'
 
 
 const App =()=> {
   return (
     <div className="App">
+      <Switch>
+        <Route path='/' exact>
+        <Signin></Signin>
+        </Route>
+      </Switch>
+  
   
     </div>
   );
 }
-
+   
 export default App;
---
+++ src/components/Signin.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import logoB from '../img/logoB.png'
+import '../Signin.scss'
+
+const Signin = () => {
+    return (
+        <div className='signin__background'>
+        <div className='signin'>
+        <div className='signin__form'></div>
+        <div className='signin__box'> </div>
+        </div>
+        </div>
+      
+    )
+}
+
+export default Signin
