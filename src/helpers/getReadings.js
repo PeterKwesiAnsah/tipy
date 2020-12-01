d178821db27e1f300228bf6df924f63592609460
Ansah1234 <ansahpeter123@yaoo.com>
2020-12-01 18:22:17 +0100

    Update

--- src/helpers/getReadings.js
+++ src/helpers/getReadings.js
@@ -5,16 +5,25 @@ const getReadings = async (firebase, id,setReadings) => {
 	//data for meter  readings
 	let rows = [];
 	let readings = [];
+	let failures=[]
 
-	//database ref to the readings node
+	//database ref to the readings node with status read
 	const readingsRef = firebase.database().ref('readings');
+
+
+	//dattabse ref to the failed node with status read 
+	const failedref=firebase.database().ref('failed')
 
 	const customers = await getCustom(firebase, id);
 
 	//Gives the snapshot of the readings node
-
 	const readingSnapshot = await readingsRef.once('value');
 
+	//give the snapshot of failed node
+	const failedSnapshot=await failedref.once('value')
+
+
+	//filter the readings from the customers node
 	Object.entries(readingSnapshot.val()).forEach(([key, value]) => {
 		//gives you an array of customers whose data has been read
 		const customer = customers.filter(
@@ -22,9 +31,24 @@ const getReadings = async (firebase, id,setReadings) => {
 		);
 		
 		rows.push(customer[0]);
-    });
+	});
+
+	//filter the failed readings from the customers node
+Object.entries(failedSnapshot.val()).forEach(([key,value])=>{
+
+const failed=customers.filter(({meterNo})=>String(key)===String(meterNo))
+
+failures.push(failed[0])
+
+})
+	
+
+
     //update meter readings for the meters read
-    update(rows,firebase)
+	update(rows,firebase,'read')
+	update(failures,firebase,'failed')
+	
+
 	rows.forEach(async ({id, name, town, meterNo, ...rest },index,array) => {
         //Update meter nodes here
 		const snapshot = await firebase
--- src/helpers/update.js
+++ src/helpers/update.js
@@ -1,8 +1,8 @@
-const update = (meters, firebase) => {
+const update = (meters, firebase,meterStatus) => {
 	let updates = {};
 	meters.forEach(({ status, id, ...rest }) => {
 		// console.log({status:'read',id,...rest})
-		updates['meters/' + id] = { status: 'read', id, ...rest };
+		updates['meters/' + id] = { status: meterStatus, id, ...rest };
 		firebase.database().ref().update(updates);
 	});
 };
